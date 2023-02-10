99964a59a2631cb6861aef3c7d6caa159fb5039f
Sky <befruitful12@gmail.com>
2023-02-09 21:57:42 -0600

    add apis

--- api/yisucang/products.js
+++ api/yisucang/products.js
@@ -63,6 +63,21 @@ const YISUCANG_ID_KEYS = [
   {
     PartnerID: '64379481',
     PartnerKey: '21c28a12-29fb-e88c-ff7d-3e8467027553'
+  },
+  // Yolanda
+  {
+    PartnerID: '77284373',
+    PartnerKey: 'fb9c267c-ec14-3a4c-44bf-7b0f1267fdd8'
+  },
+  // Janet
+  {
+    PartnerID: '83265699',
+    PartnerKey: '30a3a6e8-3c5c-1116-fecc-3a14fc5cb371'
+  },
+  // Michael
+  {
+    PartnerID: '67527241',
+    PartnerKey: '48c5de4f-860b-a2d1-bbd9-fa82a76ec542'
   }
 ]
 yisucangApis = {
