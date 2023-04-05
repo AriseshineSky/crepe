77c5f73b7b5de1376abbb8398173f8492a9b967a
SmileintheSky <befruitful12@gmail.com>
2023-04-05 09:25:38 -0500

    feat: add user route and proxy

--- proxy/index.js
+++ proxy/index.js
@@ -1,5 +1,7 @@
-exports.Product = require('./product');
-exports.Csv = require('./csv');
-exports.sheetApi = require('./sheetApi');
-exports.Freight = require('./freight');
-exports.Listing = require('./listing');
+exports.Product = require("./product");
+exports.Csv = require("./csv");
+exports.User = require("./user");
+exports.sheetApi = require("./sheetApi");
+exports.Freight = require("./freight");
+exports.Listing = require("./listing");
+
