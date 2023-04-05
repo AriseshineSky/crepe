b42d032c45827daf874eb9dd1e5a2d92a433c1c2
SmileintheSky <befruitful12@gmail.com>
2023-04-05 09:16:08 -0500

    feat: add user password to model

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
-var path = require('path');
+var path = require("path");
 
 var config = {
-  log_dir: path.join(__dirname, 'logs'),
-  debug: true
-}
+	log_dir: path.join(__dirname, "logs"),
+	debug: true,
+};
 
 // if (process.env.NODE_ENV === 'test') {
 //   config.db = 'mongodb://127.0.0.1:27017/crepe_test';
 // }
-config.db = 'mongodb://127.0.0.1:27017/crepe_test';
-// config.db = 'mongodb://mongodb:27017/crepe_test';
+// config.db = 'mongodb://127.0.0.1:27017/crepe_test';
+config.db = "mongodb://mongodb:27017/crepe_test";
 // config.db = "mongodb://root:root@172.24.0.2:27017/crepe_test";
 // console.log(process.env.NODE_ENV);
 module.exports = config;
+
--- models/user.js
+++ models/user.js
@@ -1,16 +1,25 @@
-var mongoose = require('mongoose');
-var BaseModel= require('./base_model');
+var mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+var BaseModel = require("./base_model");
 var Schema = mongoose.Schema;
 var UserSchema = new Schema({
-  name: { type: String },
-  chatId: { type: String }
-})
-
+	name: { type: String },
+	password: {
+		type: String,
+		set(val) {
+			let salt = bcrypt.genSaltSync(10);
+			let hash = bcrypt.hashSync(val, salt);
+			return hash;
+		},
+	},
+	chatId: { type: String },
+});
 UserSchema.plugin(BaseModel);
-UserSchema.pre('save', function(next) {
-  var now = new Date();
-  this.updateAt = now;
-  next();
-})
+UserSchema.pre("save", function (next) {
+	var now = new Date();
+	this.updateAt = now;
+	next();
+});
 
-mongoose.model('User', UserSchema);
+mongoose.model("User", UserSchema);
+
