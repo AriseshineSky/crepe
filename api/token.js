29c09bca99f23d46e0c15da10ad710aaf2ad9be8
SmileintheSky <befruitful12@gmail.com>
2023-04-07 10:38:19 -0500

    feat: add another G+ account

--- api/token.js
+++ api/token.js
@@ -1,47 +1,67 @@
-const axios = require('axios');
-var getToken = function(url) {
-  return new Promise((resolve, reject) => {
-    axios.post(url, {
-      "appId":"736aa3abfbfd4a7d854302ead8d5df06",
-      "appKey":"62daace5e4b073604b7e0b27"
-    }).then(
-      function(res){
-        var token = {
-          accessToken: res.data.data.accessToken,
-          expiresIn: Date.now() / 1000 + res.data.data.expiresIn
-        }
-        resolve(token);
-      }
-    ).catch(
-      function(error){
-        console.log(error)
-        reject(error)
-      }
-    );
-  })
-}
-var getToken2 = function(url) {
-  return new Promise((resolve, reject) => {
-    axios.post(url, {
-      "appId":"3e0f7d42936a4343b3ad05d1239524ca",
-      "appKey":"6399d977e4b086e9ac8877e0"
-    }).then(
-      function(res){
-        var token = {
-          accessToken: res.data.data.accessToken,
-          expiresIn: Date.now() / 1000 + res.data.data.expiresIn
-        }
-        resolve(token);
-      }
-    ).catch(
-      function(error){
-        console.log(error)
-        reject(error)
-      }
-    );
-  })
-}
+const axios = require("axios");
+var getToken = function (url) {
+	return new Promise((resolve, reject) => {
+		axios
+			.post(url, {
+				appId: "736aa3abfbfd4a7d854302ead8d5df06",
+				appKey: "62daace5e4b073604b7e0b27",
+			})
+			.then(function (res) {
+				var token = {
+					accessToken: res.data.data.accessToken,
+					expiresIn: Date.now() / 1000 + res.data.data.expiresIn,
+				};
+				resolve(token);
+			})
+			.catch(function (error) {
+				console.log(error);
+				reject(error);
+			});
+	});
+};
+var getToken2 = function (url) {
+	return new Promise((resolve, reject) => {
+		axios
+			.post(url, {
+				appId: "3e0f7d42936a4343b3ad05d1239524ca",
+				appKey: "6399d977e4b086e9ac8877e0",
+			})
+			.then(function (res) {
+				var token = {
+					accessToken: res.data.data.accessToken,
+					expiresIn: Date.now() / 1000 + res.data.data.expiresIn,
+				};
+				resolve(token);
+			})
+			.catch(function (error) {
+				console.log(error);
+				reject(error);
+			});
+	});
+};
+var getToken3 = function (url) {
+	return new Promise((resolve, reject) => {
+		axios
+			.post(url, {
+				appId: "a7939dad4f54403f85d608548a3a1113",
+				appKey: "6419b1fbe4b07fc573e5ec7e",
+			})
+			.then(function (res) {
+				var token = {
+					accessToken: res.data.data.accessToken,
+					expiresIn: Date.now() / 1000 + res.data.data.expiresIn,
+				};
+				resolve(token);
+			})
+			.catch(function (error) {
+				console.log(error);
+				reject(error);
+			});
+	});
+};
 module.exports = {
-  getToken,
-  getToken2
-}
+	getToken,
+	getToken2,
+	getToken3,
+};
+
