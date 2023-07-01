ab6f5804373b1be58b55c3db94dcd59aa9d301cb
SmileintheSky <befruitful12@gmail.com>
2023-07-01 09:45:35 -0500

    add pm2 config

--- config.js
+++ config.js
@@ -5,12 +5,10 @@ var config = {
 	debug: true,
 };
 
-// if (process.env.NODE_ENV === 'test') {
-//   config.db = 'mongodb://127.0.0.1:27017/crepe_test';
-// }
-// config.db = 'mongodb://127.0.0.1:27017/crepe_test';
-config.db = "mongodb://mongodb:27017/crepe_test";
-// config.db = "mongodb://root:root@172.24.0.2:27017/crepe_test";
-// console.log(process.env.NODE_ENV);
-config.SECRET = "fdfhfjdfdjfdjerwrereresaassa2dd@ddds";
+config.if (process.env.NODE_ENV === "production") {
+  config.db = "mongodb://127.0.0.1:27017/crepe";
+} else {
+  config.db = "mongodb://mongodb:27017/crepe";
+}
+SECRET = "fdfhfjdfdjfdjerwrereresaassa2dd@ddds";
 module.exports = config;
