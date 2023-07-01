aa4b53cc5911950df6b1de3b754bab71d9614753
SmileintheSky <befruitful12@gmail.com>
2023-07-01 09:51:34 -0500

    fix config

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ var config = {
 	debug: true,
 };
 
-config.if (process.env.NODE_ENV === "production") {
-  config.db = "mongodb://127.0.0.1:27017/crepe";
+if (process.env.NODE_ENV === "production") {
+	config.db = "mongodb://127.0.0.1:27017/crepe";
 } else {
-  config.db = "mongodb://mongodb:27017/crepe";
+	config.db = "mongodb://mongodb:27017/crepe";
 }
 SECRET = "fdfhfjdfdjfdjerwrereresaassa2dd@ddds";
 module.exports = config;
